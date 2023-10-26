4958c72b365f8bfd721c799e0ff9878527f550f4
VictoriaLedezma <vickyledezma71@gmail.com>
2023-10-25 21:47:45 -0300

    Cambio_vendedor
    hicimos la clase vendedor,

--- TpAutomotriz/Entidades/Vendedor.cs
+++ TpAutomotriz/Entidades/Vendedor.cs
@@ -8,5 +8,32 @@ namespace TpAutomotriz.Entidades
 {
     public class Vendedor : Persona
     {
+        public int Id_vendedor { get; set; }
+        public DateTime Fecha_ingreso { get; set; }
+
+
+        public int Id_categoria { get; set; }
+
+        public Vendedor()
+        {
+            Id_vendedor = 0;
+        
+            Fecha_ingreso = DateTime.MinValue;
+            Id_categoria = 0;
+
+        }
+
+        public Vendedor(int id_vent, int barrio, DateTime fec, int cat)
+        {
+            Id_vendedor = id_vent;
+            
+            Fecha_ingreso = fec;
+            Id_categoria = cat;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
